e51fcdc9b685a257686b85c83a2b43f8d029a91f
TomaszMarcinkowski86 <9454031marcinkowski@gmail.com>
2019-09-03 12:50:06 +0200

    Bazy danych NoSQL - MongoDB

---
+++ 00_hello-mongo/README.md
@@ -0,0 +1,10 @@
+# Hello Mongo
+
+To show available databases:
+```aidl
+show dbs
+```
+To use databse:
+```aidl
+use test
+```
---
+++ 06_mongo-java/src/main/java/pl/sda/nosql/mongo/Book.java
@@ -0,0 +1,20 @@
+package pl.sda.nosql.mongo;
+
+public class Book {
+
+    private String title;
+    private  int pageCount;
+
+    public Book(String title, int pageCount) {
+        this.title = title;
+        this.pageCount = pageCount;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+}
